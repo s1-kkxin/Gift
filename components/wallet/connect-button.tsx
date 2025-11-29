712251f1af7bebeb6e13c88598f621dd6ae398f8
BunzMadrid <105985148+BunzMadrid@users.noreply.github.com>
2025-11-29 18:53:51 +0800

    Add JSX type declarations inline in connect-button

--- components/wallet/connect-button.tsx
+++ components/wallet/connect-button.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      "appkit-button": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "appkit-account-button": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "appkit-network-button": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
+
 export function ConnectButton() {
   return <appkit-button />;
 }
